4309f51a8c62a53b5e0892d77d4e5c94e448cc0d
Johann Taylor <j.taylor1343@gmail.com>
2021-03-03 22:36:53 -0500

    updated Post api routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const User = require('./User');
 const Post = require('./Post');
 
+User.hasMany(Post, { //link posts to the ID column in user model via foreign key pair of user_id in Post model
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, { // defining the relationship of Post model to the User
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
