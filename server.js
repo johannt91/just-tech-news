39225d77abec231514dce2d50de0cbfdc3214e9c
Johann Taylor <j.taylor1343@gmail.com>
2021-03-10 15:51:50 -0500

    Updated login feature

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnititialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 //handlebars app template engine
 app.engine('handlebars', hbs.engine);
